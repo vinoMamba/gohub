34d920d9c6f7fa7d183f3e9b047544619bae23b7
vino <vino0908@outlook.com>
2022-10-15 12:45:58 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Hello, world.")
+	r := gin.New()
+
+	//add middleware
+	r.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(404, gin.H{
+			"message": "Not Found",
+		})
+	})
+
+	r.Run(":3000")
 }
