4da6dacc55c535e9f83134663712b6a0626d9f67
vino <vino0908@outlook.com>
2022-10-15 16:17:08 +0800

    创建user model

---
+++ app/models/user/user_model.go
@@ -0,0 +1,14 @@
+package user
+
+import "github.com/vinoMamba/gohub/app/models"
+
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
